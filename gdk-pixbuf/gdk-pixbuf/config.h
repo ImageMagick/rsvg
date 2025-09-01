11db9cfb88d8f5f9244ae304dc149679ee63f389
Dirk Lemstra <dirk@lemstra.org>
2025-09-01 17:29:31 +0200

    Created config.h file with only the values required to build.

---
+++ gdk-pixbuf/gdk-pixbuf/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define GETTEXT_PACKAGE "gdk-pixbuf"
+
+#define INCLUDE_ani
+#define INCLUDE_bmp
+#define INCLUDE_gif
+#define INCLUDE_ico
+#define INCLUDE_icns
+#define INCLUDE_jpeg
+#define INCLUDE_pcx
+#define INCLUDE_png
+#define INCLUDE_pnm
+#define INCLUDE_qtif
+#define INCLUDE_ras
+#define INCLUDE_svg
+#define INCLUDE_tga
+#define INCLUDE_tiff
+#define INCLUDE_xpm
+#define INCLUDE_xbm
